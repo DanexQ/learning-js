d54864b0021a030c97956b0c533fecd1172d2387
Daniel <danexq@gmail.com>
2022-07-29 00:24:34 +0200

    Day 22 | 1

--- 09/script.js
+++ 09/script.js
@@ -30,19 +30,43 @@ document.addEventListener('keydown', function (e) {
 });
 
 ///////////////////////////////////////////
+///////////// BTN SCROLLING ///////////////
+///////////////////////////////////////////
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// EVENT DELEGATION
+// 1. add event listener to parent element
+// 2. determine what element launched the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  const id = e.target.getAttribute('href');
+  if (e.target.classList.contains('nav__link'))
+    document.querySelector(id)?.scrollIntoView({ behavior: 'smooth' });
+});
+
+///////////////////////////////////////////
 /////////////// LECTURES //////////////////
 ///////////////////////////////////////////
 
-console.log(document.documentElement);
-console.log(document.body);
-console.log(document.head);
-
+// console.log(document.documentElement);
+// console.log(document.body);
+// console.log(document.head);
+//
 const allSections = document.querySelectorAll('.section');
-console.log(allSections);
+// console.log(allSections);
 
 const allButtons = document.getElementsByTagName('button');
-console.log(allButtons);
-console.log(document.getElementsByClassName('btn'));
+// console.log(allButtons);
+// console.log(document.getElementsByClassName('btn'));
 
 const header = document.querySelector('.header');
 const message = document.createElement('div');
@@ -70,3 +94,99 @@ header.before(message);
 document
   .querySelector('.btn--close-cookie')
   .addEventListener('click', () => message.remove());
+
+// STYLES
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+// console.log(message.style.height);
+// console.log(message.style.backgroundColor);
+// console.log(getComputedStyle(message).height);
+
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// // ATTRIBUTES
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.getAttribute('src'));
+// console.log(logo.className);
+
+// // for non-standard attributes
+// console.log(logo.getAttribute('designer'));
+// logo.setAttribute('company', 'Bankist');
+// console.log(logo.getAttribute('company'));
+
+// //data attributes
+// console.log(logo.dataset.versionNumber);
+
+// // classes
+// logo.classList.add('c');
+// logo.classList.remove('c');
+// logo.classList.toggle('c');
+// logo.classList.contains('c');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  // console.log(e.target.getBoundingClientRect());
+  console.log(`Current scroll X/Y`, window.scrollX, window.scrollY);
+  // console.log(
+  //   'height/width viewport',
+  //   document.documentElement.clientHeight,
+  //   document.documentElement.clientWidth
+  // );
+
+  //// OLD WAY TO SCROLL
+  // window.scrollTo(
+  //   s1coords.left + window.scrollX,
+  //   s1coords.top + window.scrollY
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: 'smooth',
+  // });
+
+  //// NEW WAY TO SCROLL
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// const h1 = document.querySelector('h1');
+
+// const alertH1 = function (e) {
+//   alert('addEventListener: Great! You are reading the heading! :D');
+
+//   // h1.removeEventListener('mouseenter', alertH1);
+// };
+
+// we should use this event listener below, not oldschool way and on onclick in html
+// h1.addEventListener('mouseenter', alertH1);
+
+// setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 5000);
+
+// oldschool way
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading! :D');
+// };
+
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+// console.log(randomColor());
+
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   // e.stopPropagation();
+// });
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
+// document.querySelector('.nav').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+// });
