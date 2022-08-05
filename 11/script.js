37125bd35a154a42861a5aefb0a29c6723e530e3
Daniel <danexq@gmail.com>
2022-08-06 00:24:57 +0200

    Day 30 | 2

--- 11/script.js
+++ 11/script.js
@@ -162,3 +162,57 @@ const renderCountry = function (data, neighbour = false) {
 // whereAmI(52.508, 13.381);
 // whereAmI(19.037, 72.873);
 // whereAmI(-33.933, 18.474);
+
+////////////////////////////////////
+////// BUILDING OWN PROMISES ///////
+////////////////////////////////////
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log(`Lottery draw is happening`);
+
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN ');
+    } else {
+      reject(new Error('You lost your money 💩'));
+    }
+  }, 2000);
+});
+
+lotteryPromise
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err.message));
+
+console.log('siema');
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log('I waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => console.log('I waited for 1 second'));
+
+wait(1)
+  .then(() => {
+    console.log('1 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('2 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('3 second passed');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('4 second passed');
+  });
+
+Promise.resolve('abc').then((res) => console.log(res));
